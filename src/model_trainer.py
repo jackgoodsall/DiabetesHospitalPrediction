576ca9b1b66173c18a7b09cd7a81cdaa554ff26c
jackgoodsall <jack.goodsall@gmail.com>
2025-10-20 23:02:52 +0100

    Renamed file

---
+++ src/model_trainer.py
@@ -0,0 +1,15 @@
+from components.data_splitter import split_df
+from model_builder import build_estimator
+
+
+class ModelTrainer:
+    def __init__(self, config):
+        
+
+    def _get_data(self):
+        data = 
+        
+
+    def _get_train_data(self):
+        
+    def _get_test_data(self):
