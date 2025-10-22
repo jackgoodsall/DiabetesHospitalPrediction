19b8d0afd3ef913d93a6c27f85dad2a93bfbb794
jackgoodsall <jack.goodsall@gmail.com>
2025-10-22 22:59:08 +0100

    Updated to include a functional generator for cross validation splits for kfold and stratified kfolds

--- src/components/training_splits.py
+++ src/components/training_splits.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 from typing import Tuple, Optional
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
 
+from sklearn.model_selection import (
+    KFold,
+    StratifiedKFold,
+    train_test_split  
+)
 
 def split_df(
     df: pd.DataFrame,
@@ -11,7 +16,50 @@ def split_df(
     startify: bool = False,
     group_cols=None,
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    ### Helper function to split dataframe into 2 dataframes from
-    ### main dataframe.
-    train_df, val_df = train_test_split(df, test_size=test_size)
-    return train_df, val_df
+    """
+    Function to train test split a pandas dataframe.
+
+    Can extend to stratified based on target if wanted.
+    """
+    train_df, test_df = train_test_split(df, test_size=test_size)
+    return train_df, test_df
+
+def cross_validation_splits(
+        df: pd.DataFrame,
+        target : str = None,
+        n_splits: int = 5,
+        stratergy: str = "kfold",
+        shuffle = True,
+        return_indices: bool = False,
+        random_state = None
+):
+    """
+    Yield CV splits as (train_idx, val_idx) or (train_df, val_df) depending
+    on whether return_indices is true or not.
+
+    2 Differnet stratergies available - kfold and stratified kfold.
+    """
+    if stratergy not in {"kfold", "stratified"}:
+        raise ValueError(f"Unknown stratergy {stratergy}")
+    # Split on index
+    idx = np.arange(len(df))
+
+    # K fold
+    if stratergy == "kfold":
+        splitter = KFold(n_splits, shuffle = shuffle, random_state= random_state)
+        for tr_idx, val_idx in splitter.split(idx):
+            if return_indices:
+                yield tr_idx, val_idx
+            else:
+                yield df.iloc[tr_idx], df.iloc[val_idx]
+    # Stratified k fold
+    if stratergy == "stratified":
+        assert target is not None ; "Target must not be none"
+        y = df[target].to_nummpy()
+        splitter = StratifiedKFold(n_splits, shuffle = shuffle, random_state= random_state)
+        for tr_idx, val_idx in splitter.split(idx, y):
+            if return_indices:
+                yield tr_idx, val_idx
+            else:
+                yield df.iloc[tr_idx], df.iloc[val_idx]
+
