4f9a426d3403c6d2e1016fbfbb96bce83a6d028b
jackgoodsall <jack.goodsall@gmail.com>
2026-03-01 14:23:59 +0000

    Changed the factory function to be more general and allow for easier extension to futher models in the future

--- src/model_builder.py
+++ src/model_builder.py
@@ -3,15 +3,20 @@ from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 
 
+__MODELS__ = {
+
+    "logistic_regression" : LogisticRegression,
+    "random_forest" : RandomForestClassifier,
+    "xgboost"  : XGBClassifier
+ 
+}
+
 def build_estimator(name: str, **model_configs):
     """
     Helper function for a model builder factory, builds a classifer based on the name paramater.
     Config of the model defined in model_configs.
     """
-    if name == "logreg":
-        return LogisticRegression(model_configs)
-    if name == "random_forest":
-        return RandomForestClassifier(model_configs)
-    if name == "xgboost":
-        return XGBClassifier(**model_configs)
-    return ValueError(f"Unknown model: {name}")
+    model_type = __MODELS__.get(name.strip().lower(), False)
+    if model_type == False:
+        raise ValueError(f"Unknown model: {name}")
+    return model_type(**model_configs)
